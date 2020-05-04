5bfc92832133b23ca63bce4cf88587d0469eb26d
Warren Kretzschmar <warren.kretzschmar@ki.se>
2020-05-04 15:21:55 +0200

    Fix pypi upload

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     packages=find_packages(include=["streaming_pileupy", "streaming_pileupy.*"]),
     test_suite="tests",
     url="https://github.com/winni2k/streaming_pileupy",
-    version="version='0.2.0'",
+    version="0.2.0",
     zip_safe=False,
 )
