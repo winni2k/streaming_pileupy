664170485cfec69816105a5e080218756ea08473
Warren Kretzschmar <warren.kretzschmar@ki.se>
2020-05-04 15:38:53 +0200

    Bump version: 0.2.0 → 0.2.1

--- streaming_pileupy.py
+++ streaming_pileupy.py
@@ -1,7 +1,7 @@
 """streaming_pileupy"""
 __author__ = """Warren W. Kretzschmar"""
 __email__ = "winni@warrenwk.com"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 import sys
 from collections import defaultdict
