0c84d848b768371945edcaf1f90946025a971edc
Warren Kretzschmar <warren.kretzschmar@ki.se>
2020-05-05 16:17:38 +0200

    Bump version: 0.3.2 → 0.3.3

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     packages=find_packages(include=["streaming_pileupy", "streaming_pileupy.*"]),
     test_suite="tests",
     url="https://github.com/winni2k/streaming_pileupy",
-    version="0.3.2",
+    version="0.3.3",
     zip_safe=False,
 )
